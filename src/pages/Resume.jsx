4835bb19aefd5605403392f2e8d313ab5f077ab8
David Girton <dgirton2@gmail.com>
2023-08-06 17:03:21 -0400

    page work

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,6 +1,6 @@
 
 export default function Resume() {
   return (
-    <div>Resume</div>
+    <><div>Please click the link to download a copy of my Resume</div><a href="/documents//Users/macbook/Documents/DavidGRes2023.pdf" download>Download My Resume</a></>
   )
 }
